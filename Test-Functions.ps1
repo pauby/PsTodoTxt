90c6c347d172a4c68cd7508856fe5db6acd0f136
Paul Broadwith <pauby@users.noreply.github.com>
2016-07-04 22:27:37 +0100

    Removed Test-TodoTxtProject function code (was commented out anyway) and added #TODO# on Test-ObjectProperty

--- Test-Functions.ps1
+++ Test-Functions.ps1
@@ -1,28 +1,28 @@
-﻿<# 
+﻿<#
 .SYNOPSIS
     Contains test functions related to todos.
 .DESCRIPTION
     Contains test functions that test the validity of the todo properties.
-.NOTES 
-    File Name	: Test-Functions.ps1  
+.NOTES
+    File Name	: Test-Functions.ps1
     Author		: Paul Broadwith (paul@pauby.com)
 	History		: 1.0 - 28/09/15 - Initial version
-.LINK 
+.LINK
     https://www.github.com/pauby
 #>
 
 New-Alias -Name Test-TodoTxtProject -Value Test-TodoTxtContext
 
-<# 
+<#
 .SYNOPSIS
     Tests a date.
 .DESCRIPTION
-    Tests a date for the format yyy-MM-dd. It does not test to see if the date is in the future, past or present. 
-.NOTES 
+    Tests a date for the format yyy-MM-dd. It does not test to see if the date is in the future, past or present.
+.NOTES
     Author		: Paul Broadwith (paul@pauby.com)
 	History		: 1.0 - 28/09/15 - Initial version
     TODO        : Might be easier to this via a regular expression.
-.LINK 
+.LINK
     https://www.github.com/pauby
 .PARAMETER TestDate
     The date to test. Note that this is a string and not a date object.
@@ -39,15 +39,21 @@ function Test-TodoTxtDate
     Param (
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$TodoDate
+        [string]$Date
     )
 
-    # what we do here is first of all pass the date to Get-Date and ask it to format it in yyyy-MM-dd. 
+    # what we do here is first of all pass the date to Get-Date and ask it to format it in yyyy-MM-dd.
     # If it doesn't output the same as the input the date is not in a valid format.
-    # also make sure we don't display errors if there is invalid input
+    # also make sure we don't display errors if there is invalid input; instead return $false
     $error.Clear()
-    $result = Get-Date $TodoDate -Format "yyyy-MM-dd" -ErrorAction SilentlyContinue
-    if ($result.CompareTo($TodoDate) -ne 0 -or $? -eq $false) # test if the date returned is not the same as the input or we have an error
+    try {
+        $result = Get-Date $Date -Format "yyyy-MM-dd" -ErrorAction SilentlyContinue
+    }
+    catch [System.Exception] {
+        return $false
+    }
+
+    if ($result.CompareTo($Date) -ne 0 -or $? -eq $false) # test if the date returned is not the same as the input or we have an error
     {
         $false
     }
@@ -57,20 +63,20 @@ function Test-TodoTxtDate
     }
 }
 
-<# 
+<#
 .SYNOPSIS
     Tests a todo priority.
 .DESCRIPTION
     Tests to ensure that the priority is valid.
-.NOTES 
+.NOTES
     Author		: Paul Broadwith (paul@pauby.com)
 	History		: 1.0 - 28/09/15 - Initial version
 
-    A valid priority is a single character string that is between A and Z. 
-    
+    A valid priority is a single character string that is between A and Z.
+
     TODO        : Might be easier to this via a regular expression.
-.LINK 
-    https://www.github.com/pauby/ 
+.LINK
+    https://www.github.com/pauby/
 .PARAMETER Priority
     The priority to test.
 .OUTPUTS
@@ -85,6 +91,7 @@ function Test-TodoTxtPriority
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
         [string]$Priority
     )
 
@@ -93,12 +100,12 @@ function Test-TodoTxtPriority
     ($Priority.CompareTo("A") -ge 0) -and ($Priority.CompareTo("Z") -le 0) -and ($Priority.Length -eq 1)
 }
 
-<# 
+<#
 .SYNOPSIS
     Tests the todo context.
 .DESCRIPTION
     Test the todo context is in the correct format.
-.NOTES 
+.NOTES
     Author		: Paul Broadwith (paul@pauby.com)
 	History		: 1.0 - 28/09/15 - Initial version
 
@@ -106,8 +113,8 @@ function Test-TodoTxtPriority
 
     TODO        : The function should only test a single context string so we know which one if any fail.
                   At the moment if any of the contexts fail we fail the whole test.
-.LINK 
-    https://www.github.com/pauby/ 
+.LINK
+    https://www.github.com/pauby/
 .PARAMETER Context
     The context(s) to test.
 .OUTPUTS
@@ -123,79 +130,26 @@ function Test-TodoTxtContext
     Param (
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
+        [Alias("Project")]
         [string[]]$Context
     )
 
-    # Context, as stored in the object, should just be a word with no spaces andf not start with an @
+    # Context, as stored in the object, should just be a word with no spaces and not start with an @
     # this regex is to match whitespace or an @ - if they match anything then the context is invalid
-    $regex = [regex]"(?:[@\s])"
+    $regex = [regex]"[@+\s]"
     #"(?:@\S+)"
 
     foreach ($item in $Context)
     {
         if ($regex.Match($item))
         {
-            $false
-            break
+            return $false
         }
     }
 
     # if we get here each context must be valid
     $true
 }
-
-<# 
-.SYNOPSIS
-    Tests a todo project.
-.DESCRIPTION
-    Test a todo project is valid and in the correct format.
-.NOTES 
-    Author		: Paul Broadwith (paul@pauby.com)
-	History		: 1.0 - 28/09/15 - Initial version
-
-    A valid format of project is a string with no whitespace that starts with a '+'.
-
-    TODO        : The function should only test a single context string so we know which one if any fail.
-                  At the moment if any of the contexts fail we fail the whole test.
-.LINK 
-    https://www.github.com/pauby/ 
-.PARAMETER Project
-    The project(s) to test.
-.OUTPUTS
-	Whether the project(s) are valid. Output tye is [bool]
-.EXAMPLE
-    Test-TodoProject "+computer","+home"
-
-    Tests if the projects "+computer" and "+home" are valid and outputs $true or $false.
-#>
-<#
-function Test-TodoTxtProject
-{
-    [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string[]]$Project
-    )
-
-    # Project, as stored in the object, should just be a word with no spaces andf not start with an +
-    # this regex is to match whitespace or a + - if they match anything then the project is invalid
-    $regex = [regex]"(?:[\+\s])"
-    #"(?:\+\S+)"
-
-    foreach ($item in $Project)
-    {
-        if ($regex.Match($item))
-        {
-            $false
-            break
-        }
-    }
-
-    # if we get here each context must be valid
-    $true
-}
-#>
 
 <#
 .SYNOPSIS
@@ -205,10 +159,11 @@ function Test-TodoTxtProject
 .NOTES
 	Author		: Paul Broadwith (paul@pauby.com)
 	History		: 1.0 - 26/04/16 - Initial version
-	
+
 	Credit		: Code originally taken from http://stackoverflow.com/questions/26997511/how-can-you-test-if-an-object-has-a-specific-property (author http://stackoverflow.com/users/520612/cb)
-	
+
 	Notes		: Code requires PowerShell v3 - see Credit link for v2 version
+    #TODO#      : This may not be required
 .LINK
     https://www.github.com/pauby
 .PARAMETER PARAMETER
@@ -231,21 +186,21 @@ function Test-ObjectProperty
 		[Parameter(Mandatory=$true, Position=1, ValueFromPipeline=$true)]
 		[ValidateNotNullOrEmpty()]
 		[object]$InputObject,
-		
+
 		[Parameter(Mandatory=$true)]
 		[ValidateNotNullOrEmpty()]
 		[string]$PropertyName
 	)
-	
+
 	Begin
 	{
 	}
-	
+
 	Process
 	{
 		return [bool]($_.PSobject.Properties.name -match $PropertyName)
 	}
-	
+
 	End
 	{
 	}
