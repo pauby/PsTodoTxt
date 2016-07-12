f126c4999165c6b45cf1306469b47a842bfe1ac3
Paul Broadwith <paul@pauby.com>
2016-07-12 10:16:12 +0100

    Added InModuleScope as test throws error

--- Tests/New-TodoTxtObject.Tests.ps1
+++ Tests/New-TodoTxtObject.Tests.ps1
@@ -2,15 +2,18 @@
 $here = Split-Path -Parent $here
 Import-Module $here\PsTodoTxt
 
-Describe "New-TodoTxtObject" {
+InModuleScope PSTodoTxt {
 
-    $testDate = "2016-07-01"
-    Mock Get-TodoTxtTodaysDate { return $testDate }
-    
-    It "Should return an object with the CreatedDate property" {
-        $actual = New-TodoTxtObject
-        $expected = New-Object -TypeName PSObject -Property @{ CreatedDate = $testDate }
-        Compare-Object -ReferenceObject $actual -DifferenceObject $expected | Should Be $null
+    Describe "New-TodoTxtObject" {
+
+        $testDate = "2016-07-01"
+        Mock Get-TodoTxtTodaysDate { return $testDate }
+        
+        It "Should return an object with the CreatedDate property" {
+            $actual = New-TodoTxtObject
+            $expected = New-Object -TypeName PSObject -Property @{ CreatedDate = $testDate }
+            Compare-Object -ReferenceObject $actual -DifferenceObject $expected -Property CreatedDate | Should Be $null
+        }
     }
 }
 
