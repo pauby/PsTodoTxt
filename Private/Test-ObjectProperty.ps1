b7ef3b3db6aeb14dc7dd230fd41506843fb2d8fe
Paul Broadwith <paul@pauby.com>
2016-09-01 13:51:57 +0100

    Added check for parameter type

--- Private/Test-ObjectProperty.ps1
+++ Private/Test-ObjectProperty.ps1
@@ -6,10 +6,10 @@
 .NOTES
 	Author		: Paul Broadwith (paul@pauby.com)
 	History		: 1.0 - 26/04/16 - Initial version
-
+				  1.1 - 01/09/16 - Added ValidateScript to check that input is an object
 	Credit		: Code originally taken from http://stackoverflow.com/questions/26997511/how-can-you-test-if-an-object-has-a-specific-property (author http://stackoverflow.com/users/520612/cb)
-
 	Notes		: Code requires PowerShell v3 - see Credit link for v2 version
+#Requires -Version 3
 .LINK
     https://www.github.com/pauby
 .PARAMETER InputObject
@@ -32,21 +32,20 @@ function Test-ObjectProperty
 	[OutputType([boolean])]
 	Param
 	(
-		[Parameter(Mandatory=$true, Position=1, ValueFromPipeline=$true)]
+		[Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
+		[ValidateScript( { $_.GetType().Name -eq "PSCustomObject" } )]
 		[ValidateNotNull()]
 		[object]$InputObject,
 
-		[Parameter(Mandatory=$true)]
+		[Parameter(Mandatory=$true, Position=1)]
 		[ValidateNotNullOrEmpty()]
 		[string]$PropertyName
 	)
 
-	Begin
-	{
+	Begin {
 	}
 
-	Process
-	{
+	Process	{
         # check if the object has any properties to check
         # NOTE: not entirely sure we should be using [string]::IsNullOrEmpty() here but it works whereas -eq $null doesn't
         if ([string]::IsNullOrEmpty($InputObject.PSObject.Properties)) {
@@ -59,7 +58,6 @@ function Test-ObjectProperty
 		# we should never get here
 	}
 
-	End
-	{
+	End	{
 	}
 }
