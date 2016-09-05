37d63afdc8af46fd9bbffda3ae7aed903ca6ad43
Paul Broadwith <paul@pauby.com>
2016-09-05 16:36:08 +0100

    Updated to add scripts in subfolders

--- PSTodoTxt.psm1
+++ PSTodoTxt.psm1
@@ -11,6 +11,7 @@ Set-StrictMode -Version Latest
 
 $scriptsToLoad = @(
     'Public\Import-TodoTxt.ps1',
+    'Public\Expor'
     'Public\ConvertFrom-TodoTxtString.ps1',
     'Public\Set-TodoTxt.ps1',
     'Public\ConvertTo-TodoTxtString.ps1',
@@ -24,10 +25,15 @@ $scriptsToLoad = @(
     'Private\Write-VerboseHashTable.ps1'
 )
 
-foreach ($script in $scriptsToLoad)
-{
-    Write-Verbose "Importing script file $script"
-    . (Join-Path $PSScriptRoot $script)
+$toInclude = @()                            # add the files, in the root, to be included
+$searchFolders = @('Public', 'Private')     # add the folders, from the root, that are to be searched for .ps1 files 
+$searchFolders | ForEach-Object { 
+        $toInclude += (Get-ChildItem (Join-Path -Path $PSScriptRoot -ChildPath $_) -recurse -include '*.ps1')
+}
+
+foreach ($script in $toInclude) {
+    Write-Verbose "Importing $script"
+    . $script
 }
 
 New-Alias -Name Test-TodoTxtProject -Value Test-TodoTxtContext
