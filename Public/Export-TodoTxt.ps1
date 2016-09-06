178f8edabe055d764a516c1b0d70241d84535b89
Paul Broadwith <pauby@users.noreply.github.com>
2016-09-06 22:28:22 +0100

    Clarified parameter help

--- Public/Export-TodoTxt.ps1
+++ Public/Export-TodoTxt.ps1
@@ -1,34 +1,44 @@
+
+function Export-TodoTxt
+{
 <#
 .SYNOPSIS
-    Imports todos from the todo file.
+    Exports todotxt objects.
 .DESCRIPTION
-    Reads todos from the todo file and has them converted to todo objects before output.
+    Exports todotxt, previously created with ConvertFrom-TodoTxtString,
+    to a text file. Before exporting the todotxt objects are converted
+    back to todotxt strings by calling the cmdlet
+    ConvertTo-TodoTxtString.
 .NOTES
     Author		: Paul Broadwith (paul@pauby.com)
 	History		: 1.0 - 29/04/16 - Initial version
+                : 2.0 - 06/09/16 - Completely rewritten to accept pipeline and parameter input.
 .LINK
-    https://www.github.com/pauby
+    https://www.github.com/pauby/pstodotxt
 .PARAMETER Path
-    Path to the todo file.
-.OUTPUTS
-    Output is [array]
+    Path to the todo file. If the file exists and -Append parameter is
+    not specified then the file will be overwritten.
 .EXAMPLE
-    Import-Todo c:\todo.txt
+    $todo = Import-TodoTxt -Path c:\input.txt
+    Export-TodoTxt -Todo $todo -Path c:\output.txt
 
-    Reads the todos in the todo.txt file, has them converted to objects before outputting them..
+    Converts the todotxt objects in $todo to todotxt strings and writes
+    them to the file c:\output.txt.
+.EXAMPLE
+    Import-TodoTxt -Path c:\input.txt | Export-TodoTxt -Path c:\output.txt -Append
+
+    Imports todotxt strings from c:\input.txt and then exports the file c:\output.txt
+    by appending them to the end of the file.
 #>
-
-function Import-TodoTxt
-{
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
         [ValidateNotNull()]
-        [object]
+        [object[]]
         $Todo,
 
-        [Parameter(Mandatory=$true)]
-        [ValidateScript( { Test-Path $_ } )]
+        [Parameter(Mandatory=$true,Position=1)]
+        [ValidateScript( { if ($PSBoundParameters.ContainsKey("Append")) { Test-Path $_ } else { $true } } )]
         [string]
         $Path,
 
@@ -37,32 +47,31 @@ function Import-TodoTxt
     )
 
     Begin {
-        $todoText = @()
-        $converted = @()
         $PipelineInput = -not $PSBoundParameters.ContainsKey("Todo")
-        if ($PipelineInput) {
-            Write-Verbose "We are taking data from the pipeline."
-        }
-        else {
-            Write-Verbose "We are taking data from function parameters."
-        }
     }
 
-    Process {
-        if ($PipelineInput) {
-            $pipe = $_
-        }
-        else {
-            $pipe = $Todo
-        }
-
-        $pipe | Foreach-Object {
-            $_ | ConvertTo-TodotxtString | Set-Content -Path $Path -
-
-        }
-        $
-        $text = "{0}{1}{2}{3}{4}" -f { if ($InputObject.}
-    }
+    # NOTE:
+    # in order to use $input in End{} we CANNOT have a Process{}
+    #
 
     End {
+        if ($PipelineInput) {
+            $toExport = @($input)
+        }
+        else {
+            $toExport = $Todo
+        }
+
+        Write-Verbose "We have $($toExport.count) objects in the pipeline to write to $Path."
+        if ($VerbosePreference -ne "SilentlyContinue") {
+            $toExport | ForEach-Object { Write-Verbose "Object: $_" }
+        }
+
+        if ($Append.IsPresent) {
+            $toExport | ConvertTo-TodoTxtString | Add-Content -Path $Path -Encoding UTF8
+        }
+        else {
+            $toExport | ConvertTo-TodoTxtString | Set-Content -Path $Path -Encoding UTF8
+        }
     }
+}
