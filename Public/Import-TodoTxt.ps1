7dd3347143808ebc8499a1e256e43cc91aa5165e
Paul Broadwith <pauby@users.noreply.github.com>
2016-09-06 22:32:14 +0100

    Bug fix - removed forcing output to be an array

--- Public/Import-TodoTxt.ps1
+++ Public/Import-TodoTxt.ps1
@@ -1,27 +1,27 @@
-﻿<#
+﻿function Import-TodoTxt
+{
+<#
 .SYNOPSIS
-    Imports todos from the todo file.
+    Imports todotxt strings and converts them to objects.
 .DESCRIPTION
-    Reads todos from the todo file and has them converted to todo objects before output.
+    Imports todotxt strings from the source file and converts them to objects.
 .NOTES
     Author		: Paul Broadwith (paul@pauby.com)
-	History		: 1.0 - 29/04/16 - Initial version
+	History		: 1.0 - 29/04/16 - Initial version.
+                : 1.1 - 06/09/16 - Removed forcing output to be an array and replaced return with Write-Output.
 .LINK
-    https://www.github.com/pauby
+    https://www.github.com/pauby/pstodotxt
 .PARAMETER Path
-    Path to the todo file.
+    Path to the todo file. The file must exist.
 .OUTPUTS
-    Output is [array]
+    Output is [object]
 .EXAMPLE
-    Import-Todo c:\todo.txt
+    Import-Todo -Path c:\todo.txt
 
-    Reads the todos in the todo.txt file, has them converted to objects before outputting them..
+    Reads the todotxt stings from the file c:\todo.txt and converts them to objects..
 #>
-
-function Import-TodoTxt
-{
     [CmdletBinding()]
-    [OutputType([array])]
+    [OutputType([object])]
     Param (
         [Parameter(Mandatory=$true,
                    HelpMessage='Enter the path to the todotxt file.')]
@@ -29,18 +29,14 @@ function Import-TodoTxt
         [string]$Path
     )
 
-    # Read the contents of the todo file and force it's output to be an array so we can check number returned.
-    $todoList = @()
-
     Write-Verbose "Reading todo file ($Path) contents."
     $todos = Get-Content -Path $Path -Encoding UTF8
-    if ($todos -eq $null) {
+    if ($null -eq $todos) {
         Write-Verbose "File $Path is empty."
-        return @()
+        Write-Output @()
     }
     else {
-        Write-Verbose "Read $($todos.count) todos."
-        Write-Verbose "Converting todotxt to objects"
-        return ,($todos | ConvertFrom-TodoTxtString)
+        Write-Verbose "Read $(@($todos).count) todos."
+        Write-Output $todos | ConvertFrom-TodoTxtString
     }
 }
