c91057d9e25161507bb94d0d468bf4f82c683a13
Paul Broadwith <pauby@users.noreply.github.com>
2016-09-16 09:00:13 +0100

    Initial commit

---
+++ Private/Test-TodoTxt.ps1
@@ -0,0 +1,114 @@
+﻿function Test-TodoTxt
+{
+    <#
+.SYNOPSIS
+    Tests a todotxt object.
+.DESCRIPTION
+    Tests a TodoTxt object properties for valid values.
+.NOTES
+    Author		: Paul Broadwith (paul@pauby.com)
+	History		: 1.0 - 15/09/16 - Initial version
+
+    TODO        : Add test for addons
+                  Created a custom TodoObject so that we can check the object type and therefore guarantee it was created by New-TodoTxtObject
+.LINK
+    https://www.github.com/pauby/pstodotxt
+.PARAMETER Todo
+    The TodoTxt object to set the properties of.
+.PARAMETER DoneDate
+    The done date to set. This is only validated as a date in the correct format and can be any date past, future or present. To remove this property value from the object pass $null or an empty string as the parameter value.
+.PARAMETER CreatedDate
+    The created date to set. This is only validated as a date in the correct format and can be any date past, future or present.  To remove this property value from the object pass $null or an empty string as the parameter value.
+.PARAMETER Priority
+    The priority of the todo. To remove this property value from the object pass an empty string as the parameter value.
+.PARAMETER Task
+    The tasks description of the todo. This property cannot be removed.
+.PARAMETER Context
+    The context(s) of the todo. To remove this property value from the object pass $null or an empty string as the parameter value.
+.PARAMETER Project
+    The project(s) of the todo. To remove this property value from the object pass $null or an empty string as the parameter value.
+.PARAMETER Addon
+    The addon key:value pairs of the todo. To remove this property value from the object pass $null or an empty string as the parameter value.
+.INPUTS
+	Todo object of type [object]
+.OUTPUTS
+	The modified todo object or type [object]
+.EXAMPLE
+    $todoObj = $todoObj | Set-TodoTxt -Priority "B"
+
+    Sets the priority of the $todoObj to "B" and outputs the modified todo.
+#>
+
+    [CmdletBinding()]
+    [OutputType([Object[]])]
+	Param(
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
+                                return $true
+                            }
+                            else {
+                                return Test-TodoTxtDate $_
+                            }
+                        } )]
+        [Alias("dd")]
+        [string]$DoneDate,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript( {  Test-TodoTxtDate $_ } )]
+        [Alias("cd")]
+        [string]$CreatedDate,
+
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
+                                return $true
+                            }
+                            else {
+                                return Test-TodoTxtPriority $_
+                            }
+                        } )]
+        [Alias("pri", "u")]
+        [string]$Priority,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("t")]
+        [string]$Task,
+
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
+                                return $true
+                            }
+                            else {
+                                return Test-TodoTxtContext $_
+                            }
+                        } )]
+        [Alias("c")]
+        [string[]]$Context,
+
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
+                                return $true
+                            }
+                            else {
+                                return Test-TodoTxtContext $_   # note we don't use the alias Test-TodoTxtProject here as PSScriptAnaylzer picks up on the alias
+                            }
+                        } )]
+        [Alias("p")]
+        [string[]]$Project,
+
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [Alias("a")]
+        [string[]]$Addon
+    )
+
+    Process
+    {
+        # all the parameter testing has already been done in the parameter validation so if we get here
+        # we just need to pass back $true
+
+        Write-Output $true
+   }
+}
+
+#(New-Object -TypeName PSObject -Property @{ CreatedDate = "2017-09-88"; Task = "Lets go to the Degobah system" } ) | Test-TodoTxt
---
+++ Private/Tests/Test-TodoTxt.Tests.ps1
@@ -0,0 +1,29 @@
+﻿Describe "Testing Function - $($Function.Name) - Functional Processing & Logic" {
+    InModuleScope PSTodoTxt {
+        Context "Testing mandatory parameters" {
+            It "Passes mandatory parameters with invalid or missing data" {
+                { Test-TodoTxt -CreatedDate $null } | Should throw "null"
+                { Test-TodoTxt -CreatedDate "" } | Should throw "empty"
+                { Test-TodoTxt -Task $null } | Should throw "null"
+                { Test-TodoTxt -Task "" } | Should throw "empty"
+            }
+        }
+
+        # Because this function just validates parameters using other Test- functions there
+        # is not much else to do except test the output is done correctly
+        Context "Testing output against valid input" {
+            $todoTxtArr = @( (New-Object -TypeName PSObject -Property @{ CreatedDate = "2017-09-88"; Task = "Lets go to the Degobah system" } ),
+                    (New-Object -TypeName PSObject -Property @{ DoneDate = "2015-10-09"; CreatedDate = "2016-09-15"; Task = "LEIA!"; Context = "deathstar"; Project = @("rebels", "hoth"); Addon = @{ due = "2018-09-09"} } ),
+                    (New-Object -TypeName PSObject -Property @{ CreatedDate = "2016-09-15"; Priority = "G"; Task = "You're a little short to be a stormtrooper?"; Context = "deathstar"; Project = "prison" } )
+                    )
+
+            It "Passes all tests for valid and invalid input" {
+                 { $todoTxtArr[0] | Test-TodoTxt } | Should throw "argument"
+                ($todoTxtArr[1] | Test-TodoTxt) | Should Be $true
+                ($todoTxtArr[2] | Test-TodoTxt) | Should Be $true
+                { $todoTxtArr | Test-TodoTxt } | Should throw
+                ((1..2 | ForEach-Object { $todoTxtArr[$_] }) | Test-TodoTxt) | Should Be $true
+            }
+        }
+    }
+}
