6c0dafe959c89f74a277a86effcae8c28bdad03f
Paul Broadwith <pauby@users.noreply.github.com>
2017-01-19 22:32:18 +0000

    Changed called function name

--- Public/Import-TodoTxt.ps1
+++ Public/Import-TodoTxt.ps1
@@ -37,6 +37,6 @@
     }
     else {
         Write-Verbose "Read $(@($todos).count) todos."
-        Write-Output $todos | ConvertFrom-TodoTxtString
+        Write-Output $todos | Split-TodoTxt
     }
 }
