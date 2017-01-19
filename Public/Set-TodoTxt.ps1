b374939cbced22250542bf0bb08f13cfdadaf7d8
Paul Broadwith <pauby@users.noreply.github.com>
2017-01-19 22:35:07 +0000

    Refactored code

--- Public/Set-TodoTxt.ps1
+++ Public/Set-TodoTxt.ps1
@@ -5,34 +5,38 @@
     Sets a todo's properties.
 .DESCRIPTION
     Validates and sets a todo's properties.
+
+    The function itself does not validate the Todotxt input object. It does validate the parameters.
 .NOTES
     Author		: Paul Broadwith (paul@pauby.com)
 	History		: 1.0 - 28/09/15 - Initial version
+                  1.1 - 19/01/17 - Refactored the code; removed redundant tests
 
-    Notes       : The InputObject should be created using New-TodoTxtObject so the properties are automatically created so this code DOES NOT check if they exist.
+    Notes       : The Todo should be created using so the properties are automatically created so this code DOES NOT check if they exist.
 
     TODO        : Add test for addons
-                  Created a custom TodoObject so that we can check the object type and therefore guarantee it was created by New-TodoTxtObject
 .LINK
     https://www.github.com/pauby
 .PARAMETER Todo
     The todo object to set the properties of.
 .PARAMETER DoneDate
-    The done date to set. This is only validated as a date in the correct format and can be any date past, future or present. To remove this property value from the object pass $null or an empty string as the parameter value.
+    The done date to set. This is only validated as a date in the correct format and can be any date past, future or present.
+    To remove this property value from the object pass $null or an empty string as the parameter value.
 .PARAMETER CreatedDate
-    The created date to set. This is only validated as a date in the correct format and can be any date past, future or present.  To remove this property value from the object pass $null or an empty string as the parameter value.
+    The created date to set. This is only validated as a date in the correct format and can be any date past, future or present.
+    As a todo must always have a created date you cannot remove this property value, only change it.
 .PARAMETER Priority
     The priority of the todo. To remove this property value from the object pass an empty string as the parameter value.
 .PARAMETER Task
-    The tasks description of the todo. This property cannot be removed.
+    The tasks description of the todo. As a todo must always have a task this property cannot be removed.
 .PARAMETER Context
     The context(s) of the todo. To remove this property value from the object pass $null or an empty string as the parameter value.
 .PARAMETER Project
     The project(s) of the todo. To remove this property value from the object pass $null or an empty string as the parameter value.
 .PARAMETER Addon
-    The addon key:value pairs of the todo. To remove this property value from the object pass $null or an empty string as the parameter value.
+    The addon key:value pairs of the todo. To remove this property value from the object pass $null as the parameter value.
 .INPUTS
-	Todo object of type [object]
+	Todo object of type [psobject]
 .OUTPUTS
 	The modified todo object or type [object]
 .EXAMPLE
@@ -41,7 +45,7 @@
     Sets the priority of the $todoObj to "B" and outputs the modified todo.
 #>
 
-    [OutputType([Object])]
+    [OutputType([PSObject])]
 	Param(
         [Parameter(Position=0, ValueFromPipeline=$true)] # this parameter is mandatory but we don't want to prompt for it interactively
         [ValidateNotNull()]
@@ -49,118 +53,73 @@
 
         [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
                                 return $true
-                            } 
+                            }
                             else {
-                                return Test-TodoTxtDate -Date $_  
-                            }   
+                                return Test-TodoTxtDate -Date $_
+                            }
                         } )]
-        [Alias('dd')]
+        [Alias('dd','done')]
         [string]$DoneDate,
 
-        [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
-                                return $true
-                            } 
-                            else {
-                                return Test-TodoTxtDate -Date $_  
-                            }   
-                        } )]
-
-        [Alias('cd')]
-        [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
-                                return $true
-                            } 
-                            else {
-                                return Test-TodoTxtDate -Date $_  
-                            }   
-                        } )]
-        [string]$CreatedDate,
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript( {  return Test-TodoTxtDate -Date $_ } )]
+        [Alias('cd','created')]
+        [string]$CreatedDate,       # cannot pass an ampty string to this to wipe it - needs to have a value
 
         [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
                                 return $true
-                            } 
+                            }
                             else {
-                                return Test-TodoTxtPriority -Priority $_  
-                            }   
+                                return Test-TodoTxtPriority -Priority $_
+                            }
                         } )]
         [Alias('pri', 'u')]
         [string]$Priority,
 
+        [ValidateNotNullOrEmpty()]
         [Alias('t')]
-        [string]$Task,
+        [string]$Task,  # this cannot be empty as we need a task
 
-        [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
+        [ValidateScript( { if ($null -eq $_) {
                                 return $true
-                            } 
+                            }
                             else {
-                                return Test-TodoTxtContext -Context $_  
-                            }   
+                                return Test-TodoTxtContext -Context $_
+                            }
                         } )]
         [Alias('c')]
         [string[]]$Context,
 
-        [ValidateScript( {  if ([string]::IsNullOrEmpty($_)) {
+        [ValidateScript( {  if ($null -eq $_) {
                                 return $true
-                            } 
+                            }
                             else {
                                 return Test-TodoTxtContext -Context $_
-                            }   
+                            }
                         } )]
         [Alias('p')]
         [string[]]$Project,
 
         [Alias('a')]
-        [string[]]$Addon
+        [hashtable]$Addon
     )
 
     Begin
     {
         $validParams = @('DoneDate', 'CreatedDate', 'Priority', 'Task', 'Context', 'Project', 'Addon')
-<#        $PipelineInput = -not $PSBoundParameters.ContainsKey('Todo')
-        if ($PipelineInput) {
-            Write-Verbose 'We are taking data from the pipeline.'
-        }
-        else {
-            Write-Verbose 'We are taking data from function parameters.'
-        }#>
     }
 
     Process
     {
- #       if ($PipelineInput) {
- #           $pipe = $_
- #       }
- #       else {
- #           $pipe = $Todo
- #       }
-
         $Todo | ForEach-Object {
-            # either the object passed needs to have a task property or the Task parameter needs to have been used
-            if ( (-not ($_ | Test-ObjectProperty -PropertyName 'Task')) -and (-not $PsBoundParameters.ContainsKey('Task')) )  {
-                throw [ArgumentException]'For Task not to be required to be set, the object must already have a Task property. If the object does not have a Task property you must set one first.'
-            }
-
-            # either the object passed has a CreatedDate property or the CreatedDate parameter needs to have been used
-            if ( (-not ($_ | Test-ObjectProperty -PropertyName 'CreatedDate')) -and (-not $PsBoundParameters.ContainsKey('CreatedDate')) )  {
-                $_ | Add-Member -MemberType NoteProperty -Name 'CreatedDate' -Value (Get-TodoTxtTodaysDate)
-            }
-
-            # only check for specific parameters 
+            # only check for specific parameters
             $keys = $PsBoundParameters.Keys | Where-Object { $_ -in $validParams }
 
-            # loop through each parameter and set the corresponding proprty on the todotxt object
+            # loop through each parameter and set the corresponding property on the todotxt object
             foreach ($key in $keys)
             {
-                # check to see if the property already exists
-                if (($_ | Test-ObjectProperty -PropertyName $key)) {
-                    # property already exists so just set it to the new value
-                    $_.$key = $PsBoundParameters.$key
-                    Write-Verbose "Set $key to $($_.$key)"
-                }
-                else {
-                    # property does not already exist so create it with the new value
-                    $_ | Add-Member -MemberType NoteProperty -Name $key -Value $PsBoundParameters.$key
-                    Write-Verbose "Created and set $key to $($_.$key)"
-                }
+                Write-Verbose "Set $key to $($_.$key)"
+                $_ | Add-Member -MemberType NoteProperty -Name $key -Value $PsBoundParameters.$key -Force
             }
 
             Write-Output $_
@@ -169,6 +128,6 @@
         }
    }
 
-    End { 
+    End {
     }
 }
