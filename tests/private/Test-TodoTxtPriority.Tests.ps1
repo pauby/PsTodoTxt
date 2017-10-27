3fd2c0f26262da1e4d01b1f3a2c87d00b197a9ae
Paul Broadwith <paul@pauby.com>
2017-10-27 17:50:56 +0100

    updated tests to new format

--- tests/private/Test-TodoTxtPriority.Tests.ps1
+++ tests/private/Test-TodoTxtPriority.Tests.ps1
@@ -1,25 +1,41 @@
-Describe "Testing Function - $($Function.Name) - Functional Processing & Logic" {
+$ModuleName = 'PsTodoTxt'
+
+. "$PSScriptRoot\..\shared.ps1"
+
+$thisScript = Get-TestedScript
+Import-TestedModule | Out-Null
+
+Describe "Testing Function - Test-TodoTxtDate" {
     InModuleScope PsTodoTxt {
-        It "Should throw an exception for null / empty input" {
-            { Test-TodoTxtPriority -Priority "" } | Should throw "argument is null or empty"
+        Context "Input" {
+            It "Should throw an exception for null / empty input" {
+                { Test-TodoTxtPriority -Priority "" } | Should throw "argument is null or empty"
+            }
         }
 
-        It "Should return a boolean false for invalid priority" {
-            $actual = Test-TodoTxtPriority -Priority "9"
-            $actual | Should Be $false
-            $actual | Should BeOfType boolean
-        }
+        Context "Output" {
+            It "should be false for invalid priority" {
+                $actual = Test-TodoTxtPriority -Priority "9"
+                $actual | Should Be $false
+                $actual | Should BeOfType boolean
+            }
 
-        It "Should return a boolean true for uppercase priority" {
-            $actual = Test-TodoTxtPriority -Priority "A"
-            $actual | Should Be $true
-            $actual | Should BeOfType boolean
-        }
+            It "should be true for valid priority" {
+                $actual = Test-TodoTxtPriority -Priority "A"
+                $actual | Should Be $true
+                $actual | Should BeOfType boolean
 
-        It "Should return a boolean true for lowercase priority" {
-            $actual = Test-TodoTxtPriority -Priority "z"
-            $actual | Should Be $true
-            $actual | Should BeOfType boolean
+                $actual = Test-TodoTxtPriority -Priority "z"
+                $actual | Should Be $true
+                $actual | Should BeOfType boolean
+            }
+        } # Context
+    } # InModuleScope
+
+    Context "Code Analysis" {
+
+        It 'passes all PSScriptAnalyser rules' {
+            (Invoke-ScriptAnalyzer -Path $thisScript.Path).count | Should Be 0
         }
     }
 }
