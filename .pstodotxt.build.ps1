1f24bc180c8e09ebfa0d03a1f1294f44ff630157
Paul Broadwith <paul@pauby.com>
2018-03-12 21:13:17 +0000

    Rewrote build file

--- .pstodotxt.build.ps1
+++ .pstodotxt.build.ps1
@@ -1,41 +1,23 @@
-<#$BuildOptions = @{
-    ModuleName              = 'PSTodoTxt'
-    PSGalleryApiKey         = $env:PSGALLERY_API_KEY
-    BuildPath               = "$BuildRoot\build"
-    SourcePath              = "$BuildRoot\source"
-    TestsPath               = "$BuildRoot\tests"
-    ModuleLoadPath          = "$($env:mysyncroot)\Coding\PowerShell\Modules"
+[CmdletBinding()]
+Param (
+    [ReleaseType]
+    $ReleaseType = [ReleaseType]::Unknown,
+
+    [string]
+    $GitHubUsername = $env:GITHUB_USERNAME,
+
+    [string]
+    $GitHubApiKey = $env:GITHUB_API_KEY,
+
+    [string]
+    $PSGalleryApiKey = $env:PSGALLERY_API_KEY
+)
+
+$params = @{
+    ReleaseType     = $ReleaseType
+    GitHubUserName  = $GitHubUsername
+    GitHubApiKey    = $GitHubApiKey
+    PSGalleryApiKey = $PSGalleryApiKey
 }
 
-$BuildOptions.ModuleFiles   = @((Join-Path -Path $BuildOptions.SourcePath -ChildPath "$($BuildOptions.ModuleName).psd1"),
-                                (Join-Path -Path $BuildOptions.SourcePath -ChildPath "$($BuildOptions.ModuleName).psm1"),
-                                (Join-Path -Path $BuildOptions.SourcePath -ChildPath "$($BuildOptions.ModuleName).Format.ps1xml"),
-                                'LICENSE',
-                                (Join-Path -Path $BuildOptions.SourcePath -ChildPath 'en-GB')
-                               )
-
-$ManifestOptions = @{
-    RootModule              = "$($BuildOptions.ModuleName).psm1"
-    Author                  = 'Paul Broadwith'
-    CompanyName             = 'Paul Broadwith'
-    Copyright               = "(c) 2016-$((Get-Date).Year) Paul Broadwith"
-    Description             = 'PowerShell implementation of the Todo.txt CLI'
-    PowerShellVersion       = '3.0'
-#    DotNetFrameworkVersion  = '4.5'
-#    DefaultCommandPrefix    = 'O2'
-    FormatsToProcess        = 'PSTodoTxt.Format.ps1xml'
-    FunctionsToExport      = (Get-ChildItem (Join-Path $BuildOptions.SourcePath -ChildPath "public\*.ps1") -Recurse).BaseName
-
-    Tags                    = 'Todo', 'Todo.txt'
-#    IconUri                 = ''
-    ProjectUri              = 'https://github.com/pauby/PSTodoTxt'
-    LicenseUri              = 'https://github.com/pauby/PsTodoTxt/blob/master/LICENSE'
-    ReleaseNotes            = 'https://github.com/pauby/PSTodoTxt/blob/master/CHANGELOG.md'
-}#>
-
-. $env:PSModuleBuildScript
-
-# Synopsis: The default task: make, test, clean.
-#task . Help, Test, Clean
-#task . GitStatus, InstallDependencies, Test, CodeAnalysis, Build
-
+. (Get-BHBuildScript) @params
