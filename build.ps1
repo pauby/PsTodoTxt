3d4fddb77b24514a5e8bbdf5f71f0063829cd1d5
Paul Broadwith <paul@pauby.com>
2019-01-01 17:51:30 +0000

    Initialiaze the build environment

--- build.ps1
+++ build.ps1
@@ -3,6 +3,7 @@ Param (
     $Task = 'build'
 )
 
+.\Initialize-Build.ps1 -RequiredModule 'InvokeBuild', 'PowerShellBuild' -Verbose:$VerbosePreference
 . .\tests\TestHelpers.ps1
 
 Invoke-Build -File .\.pstodotxt.build.ps1 -Task $Task -Verbose:$VerbosePreference
