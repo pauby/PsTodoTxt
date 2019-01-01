5372e83004e7f68a9ae81bc0058a52379f3f7de6
Paul Broadwith <paul@pauby.com>
2019-01-01 20:18:48 +0000

    Add import module helper directly

--- build.ps1
+++ build.ps1
@@ -8,18 +8,38 @@ $dependModules = @(
         Name = 'InvokeBuild'
     },
     @{
-        Name = 'PowerShellBuild'
-        MinimumVersion = '0.3.0-beta'
+        Name            = 'PowerShellBuild'
+        MinimumVersion  = '0.3.0-beta'
         AllowPrerelease = $true
     },
     @{
-        Name = 'Pester'
+        Name           = 'Pester'
         MinimumVersion = 4.4.3
     }
+    @{
+        Name           = 'PSScriptAnalyzer'
+        MinimumVersion = '1.17.1'
+    }
+
 )
 
 .\Initialize-Build.ps1 -RequiredModule $dependModules -Verbose:$VerbosePreference
-. .\tests\TestHelpers.ps1
+#. .\tests\TestHelpers.ps1
+
+function global:Import-HelperModuleForTesting {
+    # build the filename
+    $moduleScript = "{0}\{1}.psm1" -f $env:BHBuildOutput, $env:BHProjectName
+    if (Test-Path -Path $moduleScript) {
+        Remove-Module -Name $moduleScript -ErrorAction SilentlyContinue
+        Import-Module -Name $moduleScript -Force -ErrorAction Stop
+
+        $importedModule = Get-Module -Name $env:BHProjectName
+        Write-Verbose "Imported module '$($importedModule.Path)'"
+    }
+    else {
+        throw "Module manifest '$moduleScript' does not exist!"
+    }
+}
 
 Invoke-Build -File .\.pstodotxt.build.ps1 -Task $Task -Verbose:$VerbosePreference
 
