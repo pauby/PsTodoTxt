d13cd3d3c05fb18de76deefab624813137b9927c
Paul Broadwith <paul@pauby.com>
2019-02-07 18:52:14 +0000

    Add code to test for running as administrator

--- build.ps1
+++ build.ps1
@@ -25,9 +25,20 @@ $dependModules = @(
     }
 )
 
-.\Initialize-Build.ps1 -RequiredModule $dependModules -Verbose:$VerbosePreference
-#. .\tests\TestHelpers.ps1
+# Initialize the build environment if the session is running as Admin
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
 
+if (Test-Administrator) {
+    ..\Initialize-Build.ps1 -RequiredModule $dependModules -Verbose:$VerbosePreference
+}
+else {
+    Write-Warning "Not running as Administrator - could not initialize build environment."
+}
+
+# Used in Pester tests to import the built module and not a module already installed on the system
 function global:Import-HelperModuleForTesting {
     # build the filename
     $moduleScript = "{0}\{1}.psm1" -f $env:BHBuildOutput, $env:BHProjectName
