1ff850c9b1aea2a3f9772f64231ac66a23784381
Paul Broadwith <paul@pauby.com>
2020-02-26 17:36:11 +0000

    Add build environment initialization

--- Initialize-Build.ps1
+++ Initialize-Build.ps1
@@ -1,71 +1,51 @@
-#Requires -RunAsAdministrator
+function Test-Administrator {
+    if ($PSVersionTable.Platform -ne 'Unix') {
+        $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+        (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    } else {
+        # TODO: We are running in Linux so assume (at this stage) we have root / Admin - this needs resolved
+        $true
+    }
+}
 
-[CmdletBinding()]
-Param (
-    # Hashtable:
-    #   Same parameters as Install-Module - Name is mandatory
-    [hashtable[]]
-    $RequiredModule,
-
-    [string[]]
-    $ChocoPackage
-)
+$dependencies = @{
+    InvokeBuild         = 'latest'
+    Configuration       = 'latest'
+    PowerShellBuild     = 'latest'
+    Pester              = 'latest'
+    PSScriptAnalyzer    = 'latest'
+    PSPesterTestHelpers = 'latest'  # I don't trust this Warren guy...
+    PSDeploy            = 'latest'  # Maybe pin the version in case he breaks this...
+}
 
 # dependencies
 if (-not (Get-Command -Name 'Get-PackageProvider' -ErrorAction SilentlyContinue)) {
     $null = Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     Write-Verbose 'Bootstrapping NuGet package provider.'
     Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+} elseif ((Get-PackageProvider).Name -notcontains 'nuget') {
+    Write-Verbose 'Bootstrapping NuGet package provider.'
+    Get-PackageProvider -Name NuGet -ForceBootstrap
 }
 
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-$RequiredModule | ForEach-Object {
-    if (-not (Get-Module -Name $_.Name -ListAvailable)) {
-        Write-Verbose "Installing module '$($_.Name)'."
-        Install-Module @_ -SkipPublisherCheck -AllowClobber
-    }
-    else {
-        Write-Verbose "Module '$($_.Name)' already installed."
-    }
-    Import-Module -Name $_.Name -Force
+# Trust the PSGallery is needed
+if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
+    Write-Verbose "Trusting PowerShellGallery."
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
 
-if (@($ChocoPackage.count) -gt 0) {
-    # Check if Chocolatey is installed
-    $chocoInstalled = $true
-    try {
-        Write-Verbose 'Checking if Chocolatey is installed'
-        Invoke-Expression -Command 'choco.exe' | Out-Null
-    }
-    catch {
-        try {
-            Write-Verbose 'Chocolatey not installed. Installing.'
-            # taken from https://chocolatey.org/install
-            Set-ExecutionPolicy Bypass -Scope Process -Force
-            Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        }
-        catch {
-            $chocoInstalled = $false
-            Write-Verbose 'Could not install Chocolatey'
-        }
-    }
+Install-Module -Name PSDepend
 
-    if ($chocoInstalled) {
-        $ChocoPackage | ForEach-Object {
-            Write-Verbose "Installing '$_' package."
-            choco install $_ -y
-        }
-
-        Write-Verbose 'Refreshing the PATH'
-        refreshenv
-    }
-} #end if
+if (Test-Administrator) {
+    $dependencies | Invoke-PSDepend -Import -Install -Force
+} else {
+    Write-Warning "Not running as Administrator - could not initialize build environment."
+}
 
 # Configure git
 if ($null -eq (Invoke-Expression -Command 'git config --get user.email')) {
     Write-Verbose 'Git is not configured so we need to configure it now.'
-    Invoke-Expression -Command 'git config --global user.email "pauby@users.noreply.github.com"'
-    Invoke-Expression -Command 'git config --global user.name "pauby"'
-    Invoke-Expression -Command 'git config --global core.safecrlf false'
+    git config --global user.email "pauby@users.noreply.github.com"
+    git config --global user.name "pauby"
+    git config --global core.safecrlf false
 }
